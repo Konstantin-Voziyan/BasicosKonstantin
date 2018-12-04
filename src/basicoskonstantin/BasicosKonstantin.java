c0bbd0e9002b3d6628c240544f31aa98f0430f59
Konstantin <Konstantin@Konstantin>
2018-12-04 10:59:16 +0100

    Lógicos

--- src/basicoskonstantin/BasicosKonstantin.java
+++ src/basicoskonstantin/BasicosKonstantin.java
@@ -1,21 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basicoskonstantin;
 
-/**
- *
- * @author Konstantin
- */
 public class BasicosKonstantin {
 
-    /**
-     * @param args the command line arguments
+/**
+     * Método para mostrar ejemplos de tipos lógicos
      */
+    public static void logicos() {
+
+        //Variables para hacer comparaciones
+        int a, b, c, d, e, f, g, h;
+
+        //Asignación de valores para las comparaciones
+        a = 1;
+        b = 2;
+        c = 3;
+        e = 4;
+        f = 5;
+        g = 6;
+        h = 2;
+
+        //Mostrar por pantalla el resultado de las comparaciones.
+        System.out.println("Probado expresiones lógicas:");
+        
+        System.out.println("Si a es mayor que b, imprimira \"verdad\", de lo contrario \"falso\" : ");
+        if (a > b){
+            System.out.println("VERDAD");
+        }
+        else {
+            System.out.println("FALSO");
+        }
+        
+        System.out.println("Si b es igual a h, imprimira \"verdad\", de lo contrario \"falso\" : ");
+        if (b == h){
+            System.out.println("VERDAD");
+        }
+        else {
+            System.out.println("FALSO");
+        }
+        
+        System.out.println("Si e es distinto a f, imprimira \"verdad\", de lo contrario \"falso\" : ");
+        if (e != f){
+            System.out.println("VERDAD");
+        }
+        else {
+            System.out.println("FALSO");
+        }
+        
+        System.out.println("Si g es menor que c , imprimira \"verdad\", de lo contrario \"falso\" : ");
+        if (g < c){
+            System.out.println("VERDAD");
+        }
+        else {
+            System.out.println("FALSO");
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        logicos();  // Llamada al método
     }
     
 }
